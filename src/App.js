ef7d27a6b14cde6af103a4bdd9cb65221376e842
faresmez <133017548+faresmez@users.noreply.github.com>
2023-09-28 10:15:58 +0200

    Installation des dépendances

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return ( 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>OK</p>
         <a
           className="App-link"
           href="https://reactjs.org"
