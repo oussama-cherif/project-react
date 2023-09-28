5898519d30ceeb69a1d718275d5a0c224d0e43b4
faresmez <133017548+faresmez@users.noreply.github.com>
2023-09-28 17:03:46 +0200

    Modification CarsByBrand

--- src/core/pages/marque_voitures/CarsByBrand.js
+++ src/core/pages/marque_voitures/CarsByBrand.js
@@ -6,35 +6,48 @@ import { format } from 'date-fns';
 function CarsByBrand() {
   const { id } = useParams();
   const [cars, setCars] = useState([]);
+  const [brand, setBrand] = useState([]);
+
 
   useEffect(() => {
+    axios
+      .get(`https://formation.inow.fr/demo/api/v1/brands/${id}`)
+      .then((response) => {
+        setBrand(response.data);
+      })
+      .catch((error) => {
+        console.error('Erreur marque', error);
+      });
+  
     axios
       .get('https://formation.inow.fr/demo/api/v1/cars')
       .then((response) => {
         const filteredCars = response.data.filter((car) => car.brandID == id);
-        console.log(filteredCars);
         setCars(filteredCars);
       })
       .catch((error) => {
-        console.error('Erreur', error);
+        console.error('Erreur voitures', error);
       });
-  }, [id]);
+    }, [id]);
+  
 
-  return (
-    <div>
-      <h1>Marque : {id} </h1>
-      <ul>
-        {cars.map((car) => (
-          <li key={car.id}>
-            <p>Nom du modèle : {car.model}</p>
-            <p>Année : {format(new Date(car.dateOfCirculation), 'yyyy')}</p>
-            <p>Prix : {car.price}$</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+    return (
+        <div>
+          <h1>Marque : {brand ? brand.name : 'Chargement'}</h1>
+          <img src={`/images/${brand.image}`} alt={brand.name} />
+          <ul>
+            {cars.map((car) => (
+              <li key={car.id}>
+                <p>Nom du modèle : {car.model}</p>
+                <p>Année : {format(new Date(car.dateOfCirculation), 'yyyy')}</p>
+                <p>Prix : {car.price}$</p>
+                <p>Marque : {brand ? brand.name : 'Chargement'}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+          }    
 
 export default CarsByBrand;
 
